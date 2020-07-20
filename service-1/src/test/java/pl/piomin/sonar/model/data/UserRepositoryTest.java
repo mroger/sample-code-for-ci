5ac3e766f4a90f279734e49a688685a8404ae872
Marcos Rogerio de Oliveira <mrogerio@uolinc.com>
2020-07-19 21:50:52 -0300

    Changing tests

---
+++ service-1/src/test/java/pl/piomin/sonar/model/data/UserRepositoryTest.java
@@ -0,0 +1,27 @@
+package pl.piomin.sonar.model.data;
+
+import org.junit.Before;
+import org.junit.Test;
+import pl.piomin.sonar.model.User;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.Assert.*;
+
+public class UserRepositoryTest {
+
+    private UserRepository userRepository;
+
+    @Before
+    public void setUp() {
+        Set<User> users = new HashSet<>();
+        userRepository = new UserRepository();
+        userRepository.setUsers(users);
+    }
+
+    @Test
+    public void findByIdShouldReturnNull() {
+        userRepository.findById(1000);
+    }
+}
